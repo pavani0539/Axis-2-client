5951ea7d329d490b21c2c5d3abe6972afe871720
pavani0539 <ipavanireddy0539@gmail.com>
2017-01-12 11:52:44 -0500

    Update HighScoreServiceTest.java

--- HighScoresServiceClient/src/test/java/com/itcuties/ws/service/HighScoreServiceTest.java
+++ HighScoresServiceClient/src/test/java/com/itcuties/ws/service/HighScoreServiceTest.java
@@ -183,12 +183,7 @@ public class HighScoreServiceTest {
 		
 		System.out.println("There is "+response.getReturn()+" highscores on the list.");
 		
-		<script>//javascript code
-a=15;//initialization of variables
-b=20;
-c=a+b;//perform sum operation
-document.write("sum of numbers is: " +c);//print the result(+ is a concatenate operator)
-//</script>
+		
 
 	}
 }
