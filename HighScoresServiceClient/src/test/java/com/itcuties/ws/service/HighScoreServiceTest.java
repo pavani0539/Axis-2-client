b69ee766da21d9205e625e18957bbd72a6681146
pavani0539 <ipavanireddy0539@gmail.com>
2017-01-12 11:49:11 -0500

    Update HighScoreServiceTest.java

--- HighScoresServiceClient/src/test/java/com/itcuties/ws/service/HighScoreServiceTest.java
+++ HighScoresServiceClient/src/test/java/com/itcuties/ws/service/HighScoreServiceTest.java
@@ -188,7 +188,7 @@ a=15;//initialization of variables
 b=20;
 c=a+b;//perform sum operation
 document.write("sum of numbers is: " +c);//print the result(+ is a concatenate operator)
-</script>
+//</script>
 
 	}
 }
